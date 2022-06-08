670a931789e3109ba32c31358967c468eec7c492
Keith <69634519+cheeseStack@users.noreply.github.com>
2022-06-08 09:15:13 +0100

    destructing object and calculation examples

---
+++ index.js
@@ -0,0 +1,47 @@
+const person = {
+    firstName: 'yoshi',
+    age: 30,
+    favColor: 'green'
+}
+
+// traditional way to make constants from an object
+    // const firstName = person.firstName
+    // const favColor = person.favColor
+
+
+// destructure the object and set as constants
+const { firstName, favColor, age } = person;
+
+console.log(firstName, favColor, age);
+
+
+
+
+const nums = [7,9,3,5,2]
+
+const [a,b,c] = nums
+
+console.log(a,b,c,b,a)
+
+
+
+const getCalculations = (a,b) => {
+
+    return {
+        sum: a + b,
+        diff: Math.abs(a - b),
+        product: a * b
+    }
+}
+
+const {sum, product, diff} = getCalculations(7, 9)
+
+console.log(sum, product, diff);
+
+
+const doSomething = ({ name }) => {
+
+    console.log(name);
+}
+
+doSomething( {name: 'keith', age: 40 })
