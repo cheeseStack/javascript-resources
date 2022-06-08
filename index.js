d9e33cfb2f9370ba99a3252067b01d77d4bce2ea
Keith <69634519+cheeseStack@users.noreply.github.com>
2022-06-08 14:02:50 +0100

    index.js file showing non-destructive map and filter methods to return an array based on set parameters

--- index.js
+++ index.js
@@ -1,55 +1,81 @@
-import { ninjas, greet } from './ninjas.js'
+// filter and map methods
+
+const ninjas = [ 
+    {name: 'shaun', belt:'black'},
+    {name: 'mario', belt:'orange'},
+    {name: 'yoshi', belt:'black'},
+    {name: 'luigi', belt:'green'}
+]
 
 
-console.log(ninjas)
+// filter method: filter items out, non-destructive
+const blackBelts = ninjas.filter((ninja) => {
+    return ninja.belt == 'black'
+})
 
-greet()
-
-
-// const person = {
-//     firstName: 'yoshi',
-//     age: 30,
-//     favColor: 'green'
-// }
-
-// // traditional way to make constants from an object
-//     // const firstName = person.firstName
-//     // const favColor = person.favColor
+console.log(blackBelts);
 
 
-// // destructure the object and set as constants
-// const { firstName, favColor, age } = person;
+// map method
 
-// console.log(firstName, favColor, age);
+const names = ninjas.map((ninja) => {
+    return ninja.name
+})
 
+console.log(names);
 
-
-
-// const nums = [7,9,3,5,2]
-
-// const [a,b,c] = nums
-
-// console.log(a,b,c,b,a)
+// import { ninjas, greet } from './ninjas.js'
 
 
+// console.log(ninjas)
 
-// const getCalculations = (a,b) => {
-
-//     return {
-//         sum: a + b,
-//         diff: Math.abs(a - b),
-//         product: a * b
-//     }
-// }
-
-// const {sum, product, diff} = getCalculations(7, 9)
-
-// console.log(sum, product, diff);
+// greet()
 
 
-// const doSomething = ({ name }) => {
+// // const person = {
+// //     firstName: 'yoshi',
+// //     age: 30,
+// //     favColor: 'green'
+// // }
 
-//     console.log(name);
-// }
+// // // traditional way to make constants from an object
+// //     // const firstName = person.firstName
+// //     // const favColor = person.favColor
 
-// doSomething( {name: 'keith', age: 40 })
+
+// // // destructure the object and set as constants
+// // const { firstName, favColor, age } = person;
+
+// // console.log(firstName, favColor, age);
+
+
+
+
+// // const nums = [7,9,3,5,2]
+
+// // const [a,b,c] = nums
+
+// // console.log(a,b,c,b,a)
+
+
+
+// // const getCalculations = (a,b) => {
+
+// //     return {
+// //         sum: a + b,
+// //         diff: Math.abs(a - b),
+// //         product: a * b
+// //     }
+// // }
+
+// // const {sum, product, diff} = getCalculations(7, 9)
+
+// // console.log(sum, product, diff);
+
+
+// // const doSomething = ({ name }) => {
+
+// //     console.log(name);
+// // }
+
+// // doSomething( {name: 'keith', age: 40 })
